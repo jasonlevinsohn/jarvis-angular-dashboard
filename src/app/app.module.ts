a1915b54345f3dcd2dacfbc9e2851f0cf0be5b9e
Jason Levinsohn <jason.levinsohn@gmail.com>
2016-11-11 11:40:33 -0500

    add weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { MaterialModule } from '@angular/material';
 import { FarmgateComponent } from './farmgate/farmgate.component';
 import { AngularFireModule } from 'angularfire2';
 
+import { WeatherService } from './shared/services/index';
+import { WeatherComponent } from './weather/weather.component';
+
+
 export const firebaseConfig = {
     apiKey: 'AIzaSyDVgfyNh_wPGVAMTZ3h0Np0QvR8kbqzQ0Y',
     authDomain: 'l3-controller.firebaseapp.com',
@@ -20,16 +24,17 @@ export const firebaseConfig = {
 @NgModule({
   declarations: [
     AppComponent,
-    FarmgateComponent
+    FarmgateComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MaterialModule.forRoot(),
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
